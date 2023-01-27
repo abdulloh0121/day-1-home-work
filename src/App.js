4953ea762d758bd31877d33b8f4e677309636414
Abdulloh <abdullohabdihamedov@gmail.com>
2023-01-27 19:53:27 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-import Card from './components/Card';
+import logo from "./logo.svg";
+import "./App.css";
+import Card from "./components/Card";
+
+import { useState } from "react";
 
 function App() {
+  let [counter, setCounter] = useState(6);
+  const timer = setTimeout(() => {
+    setCounter(counter - 1);
+  }, 1000);
+
+  if (counter === -1) {
+    clearTimeout(timer);
+    setCounter(6);
+  }
+
+  let data = counter;
   return (
     <div className="App">
-      <Card/>
+      <div className="Timer_cart">
+        <p className="increaseCounter">{data}</p>
+      </div>
+      <Card />
     </div>
   );
 }
-export default App
-
+export default App;
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,79 @@
+import { useState } from "react";
+
 function Card() {
-    return (
-      <div className="card" style={{ width: "18rem" }}>
-        <img src="..." className="card-img-top" alt="..." />
+  const [input, setInput] = useState("");
+  const [todo, setTodo] = useState([
+    {
+      id: 1,
+      titel: "titel 1",
+      completed: true,
+    },
+  ]);
+
+  const addTodohandler = () => {
+    const newTodo = {
+      id: Math.random().toString(),
+      titel: input,
+    };
+    setTodo([...todo, newTodo]);
+    setInput("");
+  };
+
+  const deletehandler = (id) => {
+    const result = todo.filter((todo) => {
+      if (todo.id !== id) {
+        return todo;
+      }
+    });
+    setTodo(result);
+  };
+  return (
+    <div className="App container mt-5">
+      <div className="card">
+        <div className="card-header">
+          <input
+            value={input}
+            onChange={(evt) => setInput(evt.target.value)}
+            className="form-control"
+            type="text"
+            placeholder="Add todo"
+          />
+          <button
+            onClick={addTodohandler}
+            className="btn btn-primary form-control mt-3 "
+          >
+            Add todo
+          </button>
+        </div>
         <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
-          <a href="/" className="btn btn-primary">
-            Go somewhere
-          </a>
+          <ul className="list-group">
+            {todo.map((item) => (
+              <li
+                key={item.id}
+                className="list-group-item d-flex justify-content-between"
+              >
+                <p
+                  style={{
+                    textDecoration: todo?.completed ? "line-through" : "",
+                  }}
+                >
+                  {item.titel}
+                </p>
+                <div className="d-flex gap-3">
+                  <button className="btn btn-info text-white">edd</button>
+                  <button
+                    onClick={() => deletehandler(item.id)}
+                    className="btn btn-danger"
+                  >
+                    delete
+                  </button>
+                </div>
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
-    );
-  }
-  export default Card;
+    </div>
+  );
+}
+export default Card;
